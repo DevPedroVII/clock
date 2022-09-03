256344e1c34c733c72055a6e758875d6079cec9a
pedro Vinicius <singed161@gmail.com>
2022-09-02 22:23:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function showTime(){
-    const date = new Date();
-    const H = date.getHours();
-    const M = date.getMinutes();
-    const S = date.getSeconds();
-    const session = "AM"
+    var date = new Date();
+    var H = date.getHours();
+    var M = date.getMinutes();
+    var S = date.getSeconds();
+    var session = "AM"
 
     if(H==0) {
          H=12;
@@ -12,4 +12,16 @@ function showTime(){
         H = H -12;
         session = "PM"
     }
+
+    H = (H<10) ? "0" + H : H;
+    M = (M<10) ? "0" + M : M;
+    S = (S<10) ? "0" + S : S;
+
+    const time = H + ":" + M + ":" + S + " " + session
+
+    document.getElementById("yClockdisplay").innerHTML = time;
+    document.getElementById("yClockdisplay").textContent = time;
+    setTimeout(showTime,1000)
 }
+
+showTime();
